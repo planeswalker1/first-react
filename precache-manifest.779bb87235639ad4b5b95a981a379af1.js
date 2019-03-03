04befeafa764bab50afc9a15762fd6811f608585
Daniel Munoz <daniel.munozdev@gmail.com>
2019-03-02 20:24:04 -0800

    Updates

---
+++ precache-manifest.779bb87235639ad4b5b95a981a379af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1110dbec60ec2f6aeff",
+    "url": "/first-react/static/js/runtime~main.e1110dbe.js"
+  },
+  {
+    "revision": "a23b657ab14c3fa7ab9e",
+    "url": "/first-react/static/js/main.a23b657a.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/first-react/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "a23b657ab14c3fa7ab9e",
+    "url": "/first-react/static/css/main.ef3b2a3b.chunk.css"
+  },
+  {
+    "revision": "f0d983764ee34919ca2712e4fdb89f65",
+    "url": "/first-react/index.html"
+  }
+];
